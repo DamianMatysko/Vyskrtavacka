c7783ca4d22f7dd86ab1830ac887e76a1fe2b469
FireflySK <mada11@azet.sk>
2020-04-06 13:18:25 +0200

    Initial commit

---
+++ src/Package/Vyskrtavacka.java
@@ -0,0 +1,18 @@
+package Package;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Vyskrtavacka {
+    public int cislo;
+
+    public void Vyskrtavacka(int cislo) {
+        this.cislo = cislo;
+    }
+
+    public Set<Integer> generuj() {
+        Set<Integer> mySet = new HashSet<Integer>(); // capacity of 'numElements'
+
+
+    }
+}
